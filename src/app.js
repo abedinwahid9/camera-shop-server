a84becd356a4eeb5d81ca81ba9d8ddad2a8a489a
abedin wahid <abedinwahid9@gmail.com>
2024-12-22 03:03:43 +0600

    server setup done

---
+++ src/app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const applyMiddlewares = require("./middlewares/applyMiddlewares");
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+// middlewares
+applyMiddlewares(app);
+
+app.get("/", (req, res) => {
+  res.send({ message: "server is running" });
+});
+
+app.all("*", (req, res, next) => {
+  const error = new Error(`the requested url is invalid : ${req.url}`);
+  error.status = 404;
+  next(error);
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    message: err.message,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
